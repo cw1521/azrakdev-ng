2c8a2bb7e649ae0d6b79d87c634b35c49a443999
Christian Williams <cw1521@jagmail.southalabama.edu>
2020-12-17 18:28:30 -0600

    home links complete (initial deployment)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,21 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule
   ]
 })
 
